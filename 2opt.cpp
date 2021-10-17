da187ecb6be2b6bb1ca7eece126a98b4b18395eb
kobe-tdsr <kobe.moerman@solarracinggroningen.nl>
2021-10-17 18:30:59 +0200

    improved 2-opt > kattis 17.6

--- 2opt.cpp
+++ 2opt.cpp
@@ -2,21 +2,35 @@
 
 // https://en.wikipedia.org/wiki/2-opt
 
+void two_opt_swap(int n, vector<int> &path, int start, int end) {
+    while (end - start > 0) {
+        int temp = path[start % n];
+        path[start % n] = path[end % n];
+        path[end % n] = temp;
+        start++;
+        end--;
+    }
+}
+
+int is_path_shorter(int **g, int i, int di, int k, int dk) {
+    return (g[i][k] + g[di][dk]) - (g[i][di] + g[k][dk]);
+}
+
 void two_opt (clock_t t, int n, int** g, vector<int>& p) {
   int delta;
   int i, k, di, dk;
 
-  for (i = 0; i < n - 1; i++) {
-    di = (i + 1) % n;
-    for (k = i+1; k < n; k++) {
-      dk = (k + 1) % n;
+  for (i = 0; i < n; i++) {
+    di = p[(i+1)%n];
+    for (k = i + 1; (k + 1) % n != i; k++) {
+      dk = p[(k+1)%n];
 
       if (TIME_U(clock(), t)) return;
 
-      delta = (g[i][k]+g[di][dk]) - (g[i][di]+g[k][dk]);
+      delta = g[p[i]][p[k%n]] + g[di][dk] - (g[p[i]][di] + g[p[k%n]][dk]);
 
       if (delta < 0) {
-        reverse(begin(p)+i, begin(p)+k+1);
+        two_opt_swap(n, p, i+1, k); 
       }
     }
   }
